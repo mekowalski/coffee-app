cdea377d540c2585567e300c432c2a8a974a74ac
Malind Kowalski <malind.nikulski@gmail.com>
2019-09-02 15:57:30 -0600

    move coffees controller to namespace and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :coffees
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :coffees
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
